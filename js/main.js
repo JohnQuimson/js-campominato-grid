652505885ae600b455c45b91b72d5dd3de28997e
JohnQuimson <john.quimson777@gmail.com>
2023-11-15 16:15:44 +0100

    feat: function genera

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//Funzione che crea gli elementi
+function myCreateElement(tag, className, content) {
+  const element = document.createElement(tag);
+  element.classList.add(className);
+  element.append(content);
+  return element;
+}
+
+//Estrapolo il contenitore con class .board
+const board = document.querySelector('.board');
+
+//Ciclo for che riprende la funzione per creare 100 elementi
+for (let i = 1; i <= 100; i++) {
+  const myElement = myCreateElement('div', 'cell', i);
+  board.append(myElement);
+}
+
+const btnPlay = document.querySelector('.btn-play');
