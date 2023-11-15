ab3bfcf205ca723cba2c32187d5d397e79347c3f
JohnQuimson <john.quimson777@gmail.com>
2023-11-15 17:22:25 +0100

    feat: selezione celle

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ for (let i = 1; i <= 100; i++) {
   board.append(myElement);
 }
 
+//estraggo il bottone e il div in cui ci sono l'img e testo iniziale
 const btnPlay = document.querySelector('.btn-play');
 const initialMain = document.querySelector('.initial-main');
 
@@ -27,3 +28,12 @@ btnPlay.addEventListener('click', function () {
   board.classList.remove('hidden');
   board.classList.add('show');
 });
+
+const domCelle = document.querySelectorAll('.cell');
+
+for (let j = 0; j < domCelle.length; j++) {
+  domCelle[j].addEventListener('click', function () {
+    console.log(`Hai selezionato la cella: ${j + 1}`);
+    domCelle[j].classList.add('active-cell');
+  });
+}
